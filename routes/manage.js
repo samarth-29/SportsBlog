2ae9e1eab5750cc4acc57f1f33dd3a2fabcf526f
samarth-29 <32147591+samarth-29@users.noreply.github.com>
2018-02-06 00:12:10 -0800

    Update manage.js

--- routes/manage.js
+++ routes/manage.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.send('MANAGE');
+router.get('/articles', (req, res, next) => {
+  res.render('manage_articles', {title: 'Manage Articles'});
+});
+
+router.get('/categories', (req, res, next) => {
+  res.render('manage_categories', {title: 'Manage Categories'});
 });
 
 router.get('/articles/add', (req, res, next) => {
@@ -11,6 +15,14 @@ router.get('/articles/add', (req, res, next) => {
 
 router.get('/categories/add', (req, res, next) => {
   res.render('add_category', {title: 'Create Category'});
+});
+
+router.get('/articles/edit/:id', (req, res, next) => {
+  res.render('edit_article', {title: 'Edit Article'});
+});
+
+router.get('/categories/edit/:id', (req, res, next) => {
+  res.render('edit_category', {title: 'Edit Category'});
 });
 
 module.exports = router;
